568f807820a317ea5eb8e160a46626b80ec9f379
bitaru <aiir.diir@gmail.com>
2014-12-04 22:21:46 +0300

    Changed https -> request && binary encoding fixed

--- tasks/fontastic.js
+++ tasks/fontastic.js
@@ -1,16 +1,15 @@
 /*
- * grunt-fontastic
- * https://github.com/bitaru/grunt-fontastic
- *
- * Copyright (c) 2014 bitaru
- * Licensed under the MIT license.
- */
+* grunt-fontastic
+* https://github.com/bitaru/grunt-fontastic
+*
+* Copyright (c) 2014 bitaru
+* Licensed under the MIT license.
+*/
 
 'use strict';
 
-var https   = require('https'),
-    zlib    = require('zlib'),
-    parser  = require('./lib/parser');
+var request = require('request'),
+parser  = require('./lib/parser');
 
 module.exports = function(grunt) {
 
@@ -20,8 +19,8 @@ module.exports = function(grunt) {
   grunt.registerMultiTask('fontastic', 'Update local version of Fontastic.me font', function() {
 
     var done = this.async(),
-        start = Date.now(),
-        ready = 0,
+    start = Date.now(),
+    ready = 0,
 
     // Merge task-specific and/or target-specific options with these defaults.
     options = this.options({
@@ -41,39 +40,30 @@ module.exports = function(grunt) {
     },
 
     fetchData = function(fontId, callback){
-      var buffer = [],
-          connect = {
-            hostname: options.hostname,
-            port: 443,
-            method: 'GET',
-            path: !fontId ? getStyle : getBase + 'fonts/' + fontId + '.woff'
-          };
-
-      var req = https.request(connect, function(response){
-        var res = response;
-        if(res.headers['content-encoding'] === 'gzip'){
-          res = zlib.createGunzip();
-          response.pipe(res);
+      var connect = {
+        gzip: true,
+        uri: 'https://'+options.hostname+(!fontId ? getStyle : getBase + 'fonts/' + fontId + '.woff'),
+        headers: {
+          'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11'
         }
+      };
 
-        res
-        .on('data', function (chunk) {
-          buffer.push(chunk.toString())
-        })
-        .on('end', function () {
-          return callback(buffer.join(""));
-        })
-        .on('error', function(e) {
-          grunt.log.writeln(['Error on fetch' , !fontId ? 'Style' : 'Font',  data].join(''));
-        })
+      if(fontId){
+        connect.encoding = 'binary';
+      }
+      request( connect, function ( err, res, content ) {
+        if ( err ) {
+          grunt.log.writeln(['Error on fetch' , !fontId ? 'Style' : 'Font',  err].join(''));
+        }
+        if ( res && res.statusCode === 200 ) {
+          return callback(content);
+        }
       });
-
-      req.end();
     },
 
     createCSS = function(font, data){
       var content =
-        "@font-face{font-family:\""+font+"\";src:url(\"data:font/opentype;base64,"+new Buffer(data).toString('base64')+"\");";
+      "@font-face{font-family:\""+font+"\";src:url(\"data:font/opentype;base64,"+new Buffer(data, 'binary').toString('base64')+"\");";
       grunt.file.write(options.base64File, content)
       setReady();
     },
@@ -86,10 +76,10 @@ module.exports = function(grunt) {
 
     createStyles = function(styles){
       var oldStyles = grunt.file.read(options.stylesFile),
-          regexp = /(?:.*fontastic((?:\r|\n|.)+))/i,
-          newStyles = oldStyles.replace(regexp, '// FONTASTIC\n' + styles);
-          grunt.file.write(options.stylesFile, newStyles);
-          setReady();
+      regexp = /(?:.*fontastic((?:\r|\n|.)+))/i,
+      newStyles = oldStyles.replace(regexp, '// FONTASTIC\n' + styles);
+      grunt.file.write(options.stylesFile, newStyles);
+      setReady();
     };
 
     fetchData(null, function(data){
@@ -97,8 +87,6 @@ module.exports = function(grunt) {
       makeFont(parsedData.user, parsedData.font);
       createStyles(parsedData.styles);
     });
-
-
 
   });
 
